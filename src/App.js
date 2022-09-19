fa780d7704220542d15964342f73202c31d82139
Josh Gerlach <josh.w.gerlach@gmail.com>
2022-09-19 20:17:16 +1000

    feat: add Card style layout, with Card compenent wrapper, and wrap whole layout with Layout component

--- src/App.js
+++ src/App.js
@@ -6,18 +6,17 @@ import {
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import FavouritesPage from './pages/Favourites';
-import MainNavigation from './components/layout/MainNavigation';
+import Layout from './components/layout/Layout';
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Routes>
         <Route path='/' element={<AllMeetupsPage />} />
         <Route path='/new-meetup' element={<NewMeetupPage />} />
         <Route path='/favourites' element={<FavouritesPage />} />
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,21 +1,24 @@
 
+import Card from '../ui/Card';
 import classes from './MeetupItem.module.css';
 
 function MeetupItem(props) {
     return(
-        <li className={classes.item}>
-            <div>
-                <img className={classes.image} src={props.image} alt={props.title} />
-            </div>
-            <div className={classes.content}>
-                <h3>{props.title}</h3>
-                <address>{props.address}</address>
-                <p>{props.description}</p>
-            </div>
-            <div className={classes.actions}>
-                <button>Add Fav +</button>
-            </div>
-        </li>
+        <Card>
+            <li className={classes.item}>
+                <div>
+                    <img className={classes.image} src={props.image} alt={props.title} />
+                </div>
+                <div className={classes.content}>
+                    <h3>{props.title}</h3>
+                    <address>{props.address}</address>
+                    <p>{props.description}</p>
+                </div>
+                <div className={classes.actions}>
+                    <button>Add Favourite</button>
+                </div>
+            </li>
+        </Card>
     );
 }
 
